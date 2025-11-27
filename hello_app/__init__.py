34f22aa91c70c7fc8f8bf88ee60affa3f5cec867
gabriel.esquivelnunez <gabriel.esquivelnunez@ucr.ac.cr>
2025-11-27 16:28:03 -0600

    prueba de commit a la aplicacion central

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask  # Import the Flask class
 app = Flask(__name__)    # Create an instance of the class for our use
 #comentariodeprueba1606
+
+# prueba de commit a la aplicacion central
